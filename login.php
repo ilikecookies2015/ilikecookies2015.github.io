d88f8c623408f4f7c76cd691205db29029e1cb8f
Mathieu Pestek <ilikecookies.2015@gmail.com>
2016-12-02 20:35:19 +1100

    login test

---
+++ login.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+if (isset($_POST['submit']))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if ($username == "admin" && $password == "mypassword")
+	{
+		echo "Well done, you have logged in successfully";
+	} else {
+		echo "Sorry, the login information was incorrect";
+	}
+}
+
+?>
